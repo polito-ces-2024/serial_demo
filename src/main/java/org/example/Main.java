8ce71cf0bc59c74d6d39e99f1a341985a2ed7e79
Riccardo Bertelli <19876334+dado3272@users.noreply.github.com>
2024-05-30 14:01:30 +0200

    testing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,33 @@
 package org.example;
 import com.fazecast.jSerialComm.SerialPort;
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.openssl.PEMKeyPair;
+import org.bouncycastle.openssl.PEMParser;
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
-import java.io.File;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.PKCS8EncodedKeySpec;
-import java.security.spec.X509EncodedKeySpec;
+import java.io.StringReader;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Security;
+import java.security.interfaces.ECKey;
 import java.util.Arrays;
-import java.util.concurrent.*;
-import java.security.KeyFactory;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Base64;
+
 public class Main {
+    final protected static char[] hexArray = "0123456789abcdef".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
 
     public static String arr(byte[] m) {
 
@@ -35,66 +38,113 @@ public class Main {
         return "[" + sb.toString() + "]";
     }
 
+
+
+    public static PrivateKey readPrivateKeyFromPemString(String pemString) throws Exception {
+        try (PEMParser pemParser = new PEMParser(new StringReader(pemString))) {
+            JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
+            Object object = pemParser.readObject();
+            if (object instanceof PEMKeyPair) {
+                PEMKeyPair keyPair = (PEMKeyPair) object;
+                // Extract the private key from the key pair
+                return converter.getPrivateKey(keyPair.getPrivateKeyInfo());
+            }
+            throw new IllegalArgumentException("No private key found in PEM string: " + pemString);
+        }
+    }
+    public static KeyPair readFull(String pemString) throws  Exception {
+        try (PEMParser pemParser = new PEMParser(new StringReader(pemString))) {
+            JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
+            Object object = pemParser.readObject();
+            if (object instanceof PEMKeyPair) {
+                PEMKeyPair keyPair = (PEMKeyPair) object;
+                return new KeyPair(converter.getPublicKey(keyPair.getPublicKeyInfo()), converter.getPrivateKey(keyPair.getPrivateKeyInfo()));
+            }
+            throw new IllegalArgumentException("Invalid PEM string: " + pemString);
+        }
+    }
+    public static PublicKey readPublicKeyFromPemString(String pemString) throws IOException {
+        try (PEMParser pemParser = new PEMParser(new StringReader(pemString))) {
+            JcaPEMKeyConverter converter = new JcaPEMKeyConverter().setProvider("BC");
+            Object object = pemParser.readObject();
+            if (object instanceof SubjectPublicKeyInfo) {
+                SubjectPublicKeyInfo publicKeyInfo = (SubjectPublicKeyInfo) object;
+                return converter.getPublicKey(publicKeyInfo);
+            }
+            throw new IllegalArgumentException("Invalid PEM string: " + pemString);
+        }
+    }
     public static void main(String[] args) {
         //System.out.println(HardwareDetector.detectHardware());
         try {
+            Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
 
-        String pubkey = new String(Main.class.getClassLoader().getResourceAsStream("public-key.pem").readAllBytes());
-        System.out.println(pubkey);
-        String privKey = new String(Main.class.getClassLoader().getResourceAsStream("private-key.pem").readAllBytes());
-        System.out.println(privKey);
-        byte[] byteArray = new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-            byte[] inputCipherBytes = new byte[] {
-                    (byte)0x00, (byte)0x3d, (byte)0x29, (byte)0x31, (byte)0x01, (byte)0xfa, (byte)0x6b, (byte)0xea,
-                    (byte)0x91, (byte)0x6a, (byte)0xcf, (byte)0x49, (byte)0x54, (byte)0xd2, (byte)0xd5, (byte)0xcc,
-                    (byte)0xff, (byte)0xe9, (byte)0x69, (byte)0xac, (byte)0xa3, (byte)0x30, (byte)0xcb, (byte)0x45,
-                    (byte)0x39, (byte)0x38, (byte)0x09, (byte)0x80, (byte)0x70, (byte)0xf0, (byte)0xd9, (byte)0x4f,
-                    (byte)0x66, (byte)0xb1, (byte)0xcb, (byte)0xc3, (byte)0x45, (byte)0xc3, (byte)0x2d, (byte)0x7b,
-                    (byte)0xcd, (byte)0x7b, (byte)0x16, (byte)0x71, (byte)0xd9, (byte)0x7e, (byte)0xe9, (byte)0x89,
-                    (byte)0x2c, (byte)0x59, (byte)0x0b, (byte)0x2b, (byte)0x03, (byte)0x3d, (byte)0xf6, (byte)0x7e,
-                    (byte)0x9d, (byte)0x59, (byte)0x1c, (byte)0xf7, (byte)0x32, (byte)0xc7, (byte)0xbc, (byte)0xa3,
-                    (byte)0xee, (byte)0x46, (byte)0x2e, (byte)0xd3, (byte)0x4b, (byte)0x5c, (byte)0x7b, (byte)0x4b,
-                    (byte)0x8c, (byte)0xec, (byte)0xdb, (byte)0xf1, (byte)0xec, (byte)0xc7, (byte)0x7f, (byte)0x64,
-                    (byte)0x8d, (byte)0x2c, (byte)0x09, (byte)0x48, (byte)0x4b, (byte)0x65, (byte)0xcc, (byte)0xc3,
-                    (byte)0xf5, (byte)0xee, (byte)0x10, (byte)0x2e, (byte)0x41, (byte)0xc4, (byte)0xc7, (byte)0xb7,
-                    (byte)0x9c, (byte)0xbd, (byte)0x27, (byte)0x7c, (byte)0x46, (byte)0xf3, (byte)0xc7, (byte)0x12,
-                    (byte)0x8c, (byte)0x6b, (byte)0x49, (byte)0xa0, (byte)0xbf, (byte)0xd5, (byte)0xf6, (byte)0x3c,
-                    (byte)0xca, (byte)0x4d, (byte)0x98, (byte)0x91, (byte)0x11, (byte)0xc5, (byte)0xef, (byte)0x1f,
-                    (byte)0x14, (byte)0x01, (byte)0x47, (byte)0x91, (byte)0x3f, (byte)0x48, (byte)0x7a, (byte)0x81,
-                    (byte)0x61, (byte)0x03, (byte)0xc1, (byte)0x31, (byte)0xc2, (byte)0x27, (byte)0x71, (byte)0x42,
-                    (byte)0x31, (byte)0xc9, (byte)0x74, (byte)0xe4, (byte)0x86, (byte)0x53, (byte)0x45, (byte)0x74,
-                    (byte)0x7b, (byte)0x19, (byte)0xdd, (byte)0xc7, (byte)0xbd, (byte)0x37, (byte)0xfa, (byte)0x12,
-                    (byte)0x49, (byte)0xce, (byte)0x36, (byte)0x3e, (byte)0xf9, (byte)0x21, (byte)0x71, (byte)0x6a,
-                    (byte)0x54, (byte)0x24, (byte)0xaa, (byte)0x2c, (byte)0x41, (byte)0xda, (byte)0xd7, (byte)0x44,
-                    (byte)0x1a, (byte)0x9b, (byte)0xdb, (byte)0x51, (byte)0x0c, (byte)0x4e, (byte)0x99, (byte)0x8b,
-                    (byte)0x87, (byte)0xe4, (byte)0xbb, (byte)0x6f, (byte)0x3d, (byte)0xba, (byte)0x38, (byte)0xf9,
-                    (byte)0x14, (byte)0x4d, (byte)0x27, (byte)0xf8, (byte)0x2f, (byte)0x77, (byte)0xb1, (byte)0x71,
-                    (byte)0x8d, (byte)0x6d, (byte)0x3c, (byte)0xd6, (byte)0xa0, (byte)0xe3, (byte)0xf6, (byte)0x2a,
-                    (byte)0x1b, (byte)0x70, (byte)0x74, (byte)0xd1, (byte)0x8b, (byte)0x0f, (byte)0x4e, (byte)0x9d,
-                    (byte)0x03, (byte)0xda, (byte)0xec, (byte)0x09, (byte)0x97, (byte)0x42, (byte)0x1f, (byte)0xbe,
-                    (byte)0x73, (byte)0x9b, (byte)0xcc, (byte)0x61, (byte)0x68, (byte)0xfa, (byte)0x99, (byte)0x37,
-                    (byte)0x2e, (byte)0x1d, (byte)0x9a, (byte)0x2b, (byte)0x2b, (byte)0x8f, (byte)0xa4, (byte)0x3a,
-                    (byte)0xe4, (byte)0xff, (byte)0x9e, (byte)0xd4, (byte)0x57, (byte)0x26, (byte)0x91, (byte)0xcd,
-                    (byte)0x4a, (byte)0x76, (byte)0xbd, (byte)0xc3, (byte)0x35, (byte)0x0d, (byte)0x5c, (byte)0x14,
-                    (byte)0xb4, (byte)0x14, (byte)0x03, (byte)0x73, (byte)0x47, (byte)0x0d, (byte)0x13, (byte)0x43
-            };
+        String pubkeyDevice = new String(Main.class.getClassLoader().getResourceAsStream("public_key_ecc_device.pem").readAllBytes());
+        System.out.println(pubkeyDevice);
+        String privKeyDevice = new String(Main.class.getClassLoader().getResourceAsStream("private_key_ecc_device.pem").readAllBytes());
+        System.out.println(privKeyDevice);
+
+        String pubkeyPc = new String(Main.class.getClassLoader().getResourceAsStream("public_key_ecc.pem").readAllBytes());
+        System.out.println(pubkeyDevice);
+        String privKeyPc = new String(Main.class.getClassLoader().getResourceAsStream("private_key_ecc.pem").readAllBytes());
+        System.out.println(privKeyDevice);
+
+        PrivateKey devicePriv = readPrivateKeyFromPemString(privKeyDevice);
+
+        KeyPair demo = readFull(privKeyDevice);
+        System.out.println(demo.getPrivate());
+
+        PublicKey devicePublic = readPublicKeyFromPemString(pubkeyDevice);
+
+        KeyPair pairDevice = new KeyPair(devicePublic, devicePriv);
+
+        PrivateKey pcPriv = readPrivateKeyFromPemString(privKeyPc);
+
+        PublicKey pcPublic = readPublicKeyFromPemString(pubkeyPc);
+
+        KeyPair pairPc = new KeyPair(pcPublic, pcPriv);
+
+        System.out.println("Pc " + pairPc.getPrivate());
+        System.out.println("Pc " + pairPc.getPublic());
+
+        System.out.println("Device " + pairDevice.getPrivate());
+    System.out.println("Device " + pairDevice.getPublic());
+            ECKey privEC = (ECKey) PemUtils.readPrivateKeyFromFile("/home/riccardo/IdeaProjects/serial_demo/src/main/resources/private_key_ecc.pem", "EC");
+            System.out.println(privEC.toString());
+           // System.out.println(privEC.getParameters());
+/*
+            System.out.println("Device public " + arr(devicePublic.getEncoded()) + " length " + devicePublic.getEncoded().length);
+        System.out.println("Device priv " + arr(devicePriv.getEncoded()) + "length " + devicePriv.getEncoded().length);
+        System.out.println("PC public " + arr(pcPublic.getEncoded()) + " length " + pcPublic.getEncoded().length);
+        System.out.println("PC priv " + arr(pcPriv.getEncoded()) + "length " + pcPriv.getEncoded().length);
+
+ */
+/*
             String sanitizedPub = pubkey.
                 replace("-----BEGIN PUBLIC KEY-----", "")
                     .replaceAll(System.lineSeparator(), "")
                     .replace("-----END PUBLIC KEY-----", "");
-
+            System.out.println("RAW PUB: " +  sanitizedPub);
 
             String sanitizedPriv = privKey.
-                    replace("-----BEGIN PRIVATE KEY-----", "")
+                    replace("-----BEGIN EC PRIVATE KEY-----", "")
                     .replaceAll(System.lineSeparator(), "")
-                    .replace("-----END PRIVATE KEY-----", "");
-        //System.out.println("priv: " + sanitizedPriv);
-            System.out.println("Cypher is " + inputCipherBytes.length);
-            byte[] encodedPriv = Base64.getDecoder().decode(sanitizedPriv);
+                    .replace("-----END EC PRIVATE KEY-----", "");
+            System.out.println("Raw priv: " + sanitizedPriv);
 
-        byte[] encoded = Base64.getDecoder().decode(sanitizedPub);
-        System.out.println("Public key is " + encoded.length + " bytes");
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+            byte[] rawPriv = Base64.getDecoder().decode(sanitizedPriv);
+
+        byte[] rawPublic = Base64.getDecoder().decode(sanitizedPub);
+        System.out.println("Private key is " + rawPriv.length + " bytes");
+
+        System.out.println("Bytes of public " + arr(rawPublic));
+            System.out.println("Bytes of private " + arr(rawPriv));
+
+
+
+*/
+        //KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        /*
         X509EncodedKeySpec keySpec = new X509EncodedKeySpec(encoded);
 
             PKCS8EncodedKeySpec privatekeySpec = new PKCS8EncodedKeySpec(encodedPriv);
@@ -132,13 +182,8 @@ public class Main {
             System.out.println("Decrypted encrypted" + arr(decryptedData2));
 
 
-        } catch (IOException | NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        } catch (InvalidKeySpecException e) {
-            throw new RuntimeException(e);
-        } catch (NoSuchPaddingException e) {
-            throw new RuntimeException(e);
-        } catch (InvalidKeyException | IllegalBlockSizeException | BadPaddingException e) {
+         */
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
